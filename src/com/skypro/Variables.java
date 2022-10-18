d00002fd50cf16ec66e99b9e920cbda31061fed5
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-10-18 20:58:11 +0500

    First Commit

---
+++ src/com/skypro/Variables.java
@@ -0,0 +1,9 @@
+package com.skypro;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Variables");
+
+        System.out.println("HomeWork Complete");
+    }
+}
